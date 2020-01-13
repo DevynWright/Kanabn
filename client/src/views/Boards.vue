c0626808be37e97560613317397f9808cc3a4aca
DevynWright <Wrightdevyn@gmail.com>
2020-01-13 09:26:44 -0700

    finished

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -15,7 +15,7 @@
         <button type="submit">Create Board</button>
       </form>
     </div>
-    <div id="chalkboard" :style="{ 'background-image': 'url(https://img.glyphs.co/img?src=aHR0cHM6Ly9zMy5tZWRpYWxvb3QuY29tL3Jlc291cmNlcy9EYXJrLUNydW1wbGVkLVBhcGVyLVRleHR1cmVzLVByZXZpZXctMS5qcGc&q=90&enlarge=true&h=1036&w=1600)' }">
+    <div id="chalkboard" :style="{ 'background-image': 'url(https://img.glyphs.co/img?src=aHR0cHM6Ly9zMy5tZWRpYWxvb3QuY29tL3Jlc291cmNlcy9EYXJrLUNydW1wbGVkLVBhcGVyLVRleHR1cmVzLVByZXZpZXctMS5qcGc&q=90&enlarge=true&h=1036&w=1600)' }" class="col-4">
      <div v-for="board in boards" :key="board._id">
         <router-link :to="{name: 'board', params: {boardId: board._id}}"><b style="color: white">{{board.title}}</b></router-link>
         <i @click.prevent="deleteBoard(board)" style="color: red" class="fas fa-trash-alt"></i>
